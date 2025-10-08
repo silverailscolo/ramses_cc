ab06f890000f9576fd23c300bf8afc42c1df0ab3
Willem <willem@homeassistant.local>
2025-10-08 17:58:14 +0200

    Update number of services in tests_old

--- tests/tests_old/test_services.py
+++ tests/tests_old/test_services.py
@@ -81,7 +81,7 @@ _CALL_LATER_DELAY: Final = 0  # from: custom_components.ramses_cc.broker.py
 
 NUM_DEVS_BEFORE = 3  # HGI, faked THM, faked REM
 NUM_DEVS_AFTER = 15  # proxy for success of cast_packets_to_rf()
-NUM_SVCS_AFTER = 29  # proxy for success, platform services included since 0.51.8
+NUM_SVCS_AFTER = 32  # proxy for success, platform services included since 0.51.8
 NUM_ENTS_AFTER = 47  # proxy for success
 NUM_ENTS_AFTER_ALT = (
     NUM_ENTS_AFTER - 9
@@ -862,7 +862,7 @@ TESTS_SET_ZONE_CONFIG = {
     "00": {},
     "01": {
         "min_temp": 15,
-        "max_temp": 28,
+        "max_temp": 31,
     },
     "09": {
         "min_temp": 5,
