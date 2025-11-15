13ff55668d4df359790576bb9e53a4b79e8eef46
Egbert Broerse <dcc2@ebroerse.nl>
2025-11-15 16:32:30 +0100

    rollbach (unrelated) test val

--- tests/tests_old/test_init_data.py
+++ tests/tests_old/test_init_data.py
@@ -51,7 +51,7 @@ EXPECTED_ENTITIES = [  # TODO: add OTB entities, adjust list when adding sensors
 # fmt: on
 
 NUM_DEVS_SETUP = 1  # HGI (before casting packets to RF)
-NUM_DEVS_AFTER = 9  # proxy for success of cast_packets_to_rf()
+NUM_DEVS_AFTER = 13  # proxy for success of cast_packets_to_rf()
 # adjust NUM_DEVS_AFTER when adding sensors etc. was: 9
 NUM_SVCS_AFTER = 31  # proxy for success, platform services included since 0.51.8
 NUM_ENTS_AFTER = 43  # proxy for success
