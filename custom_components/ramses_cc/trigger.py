55f3f244143417d7e1d2ca9fcb47031ada0b1904
Egbert Broerse <dcc2@ebroerse.nl>
2026-03-08 19:39:49 +0100

    clean trigger, init

--- custom_components/ramses_cc/trigger.py
+++ custom_components/ramses_cc/trigger.py
@@ -1,23 +1,32 @@
 """Provides triggers for Ramses RF."""
 
-from homeassistant.const import STATE_HOME
+import logging
+
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.trigger import (
-    Trigger,
-    make_entity_origin_state_trigger,
-    make_entity_target_state_trigger,
-)
+from homeassistant.exceptions import HomeAssistantError
+from homeassistant.helpers.entity import get_supported_features
+from homeassistant.helpers.trigger import Trigger, make_entity_target_state_trigger
 
 from .const import DOMAIN
 
+_LOGGER = logging.getLogger(__name__)
+
+
+def supports_feature(hass: HomeAssistant, entity_id: str, features: int) -> bool:
+    """Test if an entity supports the specified features."""
+    try:
+        return bool(get_supported_features(hass, entity_id) & features)
+    except HomeAssistantError:
+        return False
+
+
 TRIGGERS: dict[str, type[Trigger]] = {
-    "ramses_cc_learn": make_entity_target_state_trigger(DOMAIN, STATE_HOME),
-    "ramses_cc_regex_match": make_entity_origin_state_trigger(
-        DOMAIN, from_state=STATE_HOME
-    ),
+    f"{DOMAIN}_learn": make_entity_target_state_trigger(DOMAIN, "learn"),
+    f"{DOMAIN}_regex_match": make_entity_target_state_trigger(DOMAIN, "regex"),
 }
 
 
 async def async_get_triggers(hass: HomeAssistant) -> dict[str, type[Trigger]]:
     """Return the triggers for Ramses RF."""
+    _LOGGER.debug("EBR RamsesTriggers fetched")
     return TRIGGERS
