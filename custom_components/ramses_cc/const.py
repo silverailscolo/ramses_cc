b4f0448831b0c63b5e8714b7f539e2cb2b38babf
Egbert Broerse <dcc2@ebroerse.nl>
2026-06-01 23:16:20 +0200

    fix relocated _rf ramses path

--- custom_components/ramses_cc/const.py
+++ custom_components/ramses_cc/const.py
@@ -7,9 +7,9 @@ from typing import Final
 
 from homeassistant.const import CONF_SCAN_INTERVAL as CONF_SCAN_INTERVAL
 
+from ramses_rf.protocol.ramses import _2411_PARAMS_SCHEMA as _2411_PARAMS_SCHEMA
 from ramses_rf.schemas import SZ_SCHEMA as SZ_SCHEMA
 from ramses_tx.const import SZ_IS_EVOFW3 as SZ_IS_EVOFW3
-from ramses_tx.ramses import _2411_PARAMS_SCHEMA as _2411_PARAMS_SCHEMA
 from ramses_tx.schemas import (
     SZ_BOUND_TO as SZ_BOUND_TO,
     SZ_BUFFER_CAPACITY as SZ_BUFFER_CAPACITY,
