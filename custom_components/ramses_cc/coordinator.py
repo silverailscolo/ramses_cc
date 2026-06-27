4239a5696756815705f18928a9339ea058415faf
Egbert Broerse <dcc2@ebroerse.nl>
2026-06-27 16:51:19 +0200

    test serial_port for dict

--- custom_components/ramses_cc/coordinator.py
+++ custom_components/ramses_cc/coordinator.py
@@ -104,12 +104,12 @@ class RamsesCoordinator(DataUpdateCoordinator):
         # Redact port details for safe exchange of logs
         print_options = deepcopy(dict(self.options))  # need an extra copy
         if print_options.get("serial_port", None) is not None:
-            if (
-                print_options.get("serial_port")
-                .get("port_name", "")
-                .startswith("mqtt://")
-            ):
-                print_options["serial_port"]["port_name"] = "mqtt://usr:pwd(at)url:1883"
+            ser_port = print_options.get("serial_port", "")
+            if isinstance(ser_port, dict):
+                if ser_port.get("port_name", "").startswith("mqtt://"):
+                    print_options["serial_port"]["port_name"] = (
+                        "mqtt://usr:pwd(at)url:1883"
+                    )
         _LOGGER.debug("Config = %s", print_options)
 
         self.client: Gateway | None = None
