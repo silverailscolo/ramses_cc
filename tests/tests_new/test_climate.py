0fcbd5908127b02ce70bbc607b2d17d679cbbfa7
Egbert Broerse <dcc2@ebroerse.nl>
2026-08-08 10:00:07 +0200

    fix moved _rf const imports

--- tests/tests_new/test_climate.py
+++ tests/tests_new/test_climate.py
@@ -31,11 +31,11 @@ from custom_components.ramses_cc.const import (
     PRESET_TEMPORARY,
     SZ_KNOWN_LIST,
 )
+from ramses_rf.const import SZ_MODE, SZ_SETPOINT, SZ_SYSTEM_MODE
 from ramses_rf.devices import HvacVentilator
 from ramses_rf.models import TemperatureState
 from ramses_rf.systems.tcs import Evohome
 from ramses_rf.systems.zones import Zone
-from ramses_tx.const import SZ_MODE, SZ_SETPOINT, SZ_SYSTEM_MODE
 from ramses_tx.exceptions import ProtocolSendFailed, TransportError
 
 # Constants
